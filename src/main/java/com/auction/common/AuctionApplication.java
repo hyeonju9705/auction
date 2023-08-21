62a41715658c99896d8eff1664f72c66a165c81c
정현주 <jhj6185@douzone.com>
2023-08-21 11:42:36 +0900

    db연결 완료

--- src/main/java/com/auction/common/AuctionApplication.java
+++ src/main/java/com/auction/common/AuctionApplication.java
@@ -1,13 +1,18 @@
 package com.auction.common;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import static org.springframework.boot.SpringApplication.*;
 
 @SpringBootApplication
 public class AuctionApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AuctionApplication.class, args);
+    public static void main(String[] args) throws Exception {
+        ApplicationContext context = run(AuctionApplication.class, args); // 애플리케이션 메인 클래스 실행
+        // PostgreSQLRunner를 직접 실행하려는 경우
+        PostgreSQLRunner postgreSQLRunner = context.getBean(PostgreSQLRunner.class);
+        postgreSQLRunner.run(null); // 또는 필요한 ApplicationArguments 객체를 전달
     }
 
 }
---
+++ src/main/java/com/auction/common/PostgreSQLRunner.java
@@ -0,0 +1,35 @@
+package com.auction.common;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostgreSQLRunner implements ApplicationRunner {
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        try (Connection connection = dataSource.getConnection()){
+            System.out.println(dataSource.getClass());
+            System.out.println(connection.getMetaData().getURL());
+            System.out.println(connection.getMetaData().getUserName());
+
+            Statement statement = connection.createStatement();
+            String sql = "SELECT * FROM t_auction";
+            statement.execute(sql);
+        }
+        jdbcTemplate.execute("insert into t_auction(category_no, title, contents, is_primary) values (1,'ㅈㅔ목','내용','F')");
+    }
+}
